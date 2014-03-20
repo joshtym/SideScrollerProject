56708a588f6ca785802182d88c7d2a3e02377d73
Josh Tymburski <josh.tymburski@uleth.ca>
2014-03-19 19:36:37 -0700

    Fixed wrong header and added empty classes

--- include/GameScreen.h
+++ include/GameScreen.h
@@ -1,7 +1,17 @@
 #ifndef GAMESCREEN_H
 #define GAMESCREE_H
 
-class GameScreen
+#include "GameScreenInterface.h"
+
+class GameScreen : public GameScreenInterface
 {
+	public:
+		GameScreen();
+		~GameScreen();
+		
+		void loadContent();
+		void unloadContent();
+		void updateContent(ALLEGRO_EVENT ev);
+		void draw(ALLEGRO_DISPLAY*);
 };
 #endif
---
+++ src/GameScreen.cpp
@@ -0,0 +1,21 @@
+#include "GameScreen.h"
+
+GameScreen::GameScreen()
+{
+}
+
+GameScreen::~GameScreen()
+{
+}
+
+void GameScreen::loadContent()
+{
+}
+
+void GameScreen::updateContent(ALLEGRO_EVENT ev)
+{
+}
+
+void GameScreen::draw(ALLEGRO_DISPLAY *display)
+{
+}

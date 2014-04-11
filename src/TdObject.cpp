6a4cf822c5726f97a2f7bb5920e189cb2754e7d6
Nathen Erickson <erickson.nathen@gmail.com>
2014-04-11 16:15:19 -0700

    changed 2d to td

---
+++ src/TdObject.cpp
@@ -0,0 +1,42 @@
+#include "TdObject.h"
+
+ 
+TdObject::TdObject()
+{
+	
+	
+}
+
+int TdObject::getX()
+{
+	return x;
+}
+int TdObject::getY()
+{
+	return y;
+}
+int TdObject::getYHeight()
+{
+	return getYHeight;
+}
+int TdObject::getXWidth()
+{
+	return xWidth;
+}
+
+int TdObject::setX(int set)
+{
+	x = set;
+}
+int TdObject::setY(int set)
+{
+	y = set;
+}
+int TdObject::setYHeight(int set)
+{
+	yHeight = set;
+}
+int TdObject::setXWidth(int set)
+{
+	xWidth = set;
+}

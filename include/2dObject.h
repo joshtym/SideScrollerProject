2cd5c2d4597b8329e2eeab62fb2b681ceae5a1c6
Nathen Erickson <erickson.nathen@gmail.com>
2014-04-11 15:47:52 -0700

    objectposition header

---
+++ include/2dObject.h
@@ -0,0 +1,20 @@
+#ifndef TWODOBJECT_DETECTION_H
+#define TWODOBJECT_DETECTION_H
+
+
+class 2dObject
+{
+	private:
+		int x;
+		int y;
+		int yHeight;
+		int xWidth;
+	public:
+		int getX();
+		int getY();
+		int getYHeight();
+		int getXHeight();
+}
+
+
+#endif

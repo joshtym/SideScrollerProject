eeeafb3dda6180047a3d80a44eccdcee3099b87d
Brock <user@ubuntu.(none)>
2014-04-15 03:54:04 -0600

    Commented Spike

--- include/Spike.h
+++ include/Spike.h
@@ -3,11 +3,23 @@
 
 #include "ScrollerObstacle.h"
 
+/// Class: Spike
+/**
+ * Purpose: Deadly spike for use in game 
+**/
 class Spike : public ScrollerObstacle
 {	
 	public:
+		///Constructs a Spike
 		Spike();
+		
+		///Destructs a Spike
 		~Spike();
+
+		/// Load Scroller obstacle
+		/**
+ 		* Purpose: Loads the Obstacle assets  
+		**/
 		void loadScrollerObstacle();
 };
 #endif

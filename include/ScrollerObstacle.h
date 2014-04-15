5c650fbcd828845fc35a30658c6c111de243e0f2
Brock <user@ubuntu.(none)>
2014-04-15 03:40:22 -0600

    Commented ScrollerObstacle.h

--- include/ScrollerObstacle.h
+++ include/ScrollerObstacle.h
@@ -6,16 +6,61 @@
 #include <cstdlib>
 #include "ObjectDimensions.h"
 
+
+/// Class: Scroller Obstacle
+/**
+ * Purpose: Interface for controlling how 
+ * obstacles are loaded, drawn, and updated   
+**/
 class ScrollerObstacle
 {
 	public:
+		/// Load Scroller obstacle
+		/**
+ 		* Purpose: Loads the Image file for later display   
+		**/
 		virtual void loadScrollerObstacle() = 0;
+
+		/// Unloads Scroller obstacle
+		/**
+ 		* Purpose: unloads the assets for memory freeing   
+		**/
 		virtual void unloadScrollerObstacle();
+
+		/// Update Scroller obstacle
+		/**
+ 		* Purpose: Updates the location on obstacle   
+		**/
 		virtual void update(double);
+
+		/// Draw Scroller obstacle
+		/**
+ 		* Purpose: Draws the Image file to screen  
+		**/
 		virtual void draw(double);
+
+		/// Get Obstacles Dimensions
+		/**
+ 		* Purpose: Gets the current dimensions for use by 			* collision detection  
+		**/
 		virtual ObjectDimensions& getCurrentDimensions();
+
+		/// Reset X And Y
+		/**
+ 		* Purpose: Resets X and Y 
+		**/
 		void resetXAndYValues();
+
+		/// getIsDeadly
+		/**
+ 		* Purpose: Returns isdeadly status 
+		**/
 		bool getIsDeadly();
+
+		/// getIsBeingDrawn
+		/**
+ 		* Purpose: Returns isBeingDrawn status 
+		**/
 		bool getIsBeingDrawn();
 		
 	protected:
